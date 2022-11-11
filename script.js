8e3023e414f1721b0b7895d17d5acd1a1ef649e7
adelademarku <adelademarku@email.com>
2022-11-11 15:48:19 +0100

    ok

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+/*collego java con html */
+
+let containerPrincipale= document.querySelector("containerN")
+
+
+
 /*stampato numeri da 1 a 100 */
 
 for(let i = 0; i <= 100 ; i++){
-    console.log(i);
+    console.log(i); containerPrincipale.innerHTML + '<div class="containerN"><div class="square"><p>i</p></div></div>'
+
 
    /*chiesto se il numero è multiplo di 3 e 5*/ 
 if(i % 3 == 0 || i & 5 == 0) {console.log("FizzBuzz");}
